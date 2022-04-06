e74a2e99100871825e10a8296b225448b87d44d1
johannadesouza <johanna.ornbrand@edu.newton.se>
2022-04-06 12:40:08 +0200

    Alla stages klara

---
+++ src/main/java/com/example/frontend/startForCustomerController.java
@@ -0,0 +1,64 @@
+package com.example.frontend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.shape.Rectangle;
+
+public class startForCustomerController {
+
+        @FXML
+        private Button bookTicketButton;
+        @FXML
+        private ComboBox<?> chooseMovie;
+        @FXML
+        private ComboBox<?> chooseTime;
+        @FXML
+        private DatePicker dateOfbooking;
+        @FXML
+        private Rectangle seatEight;
+        @FXML
+        private Rectangle seatEighteen;
+        @FXML
+        private Rectangle seatEleven;
+        @FXML
+        private Rectangle seatFifteen;
+        @FXML
+        private Rectangle seatFive;
+        @FXML
+        private Rectangle seatFour;
+        @FXML
+        private Rectangle seatFourteen;
+        @FXML
+        private Rectangle seatNine;
+        @FXML
+        private Rectangle seatOne;
+        @FXML
+        private Rectangle seatSeven;
+        @FXML
+        private Rectangle seatSeventeen;
+        @FXML
+        private Rectangle seatSix;
+        @FXML
+        private Rectangle seatSixteen;
+        @FXML
+        private Rectangle seatTen;
+        @FXML
+        private Rectangle seatThirteen;
+        @FXML
+        private Rectangle seatThree;
+        @FXML
+        private Rectangle seatTwelve;
+        @FXML
+        private Rectangle seatTwo;
+
+
+
+        @FXML
+        void bookTicketOnAction(ActionEvent event) {
+
+        }
+    }
+
