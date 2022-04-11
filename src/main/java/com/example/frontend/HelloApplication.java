813769cc2c49675042471e5aa8f681a1540b6867
Killian Walsh <Killian.walsh123@hotmail.com>
2022-04-11 16:00:46 +0200

    Infogade villkor för Login knappen, som skickar login request till backend via authorize endpoint.

--- src/main/java/com/example/frontend/HelloApplication.java
+++ src/main/java/com/example/frontend/HelloApplication.java
@@ -2,7 +2,6 @@ package com.example.frontend;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -11,13 +10,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        Scene scene1 = new Scene(root);
-        stage.setScene(scene1);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }
